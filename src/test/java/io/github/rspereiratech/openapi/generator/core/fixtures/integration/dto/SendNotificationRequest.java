e07235b85a9c3bbcc32622edc532dac36484992d
Rui Pereira <ruichaves21@gmail.com>
2026-03-17 18:50:13 +0000

    chore: add MIT license header to test fixture files

--- src/test/java/io/github/rspereiratech/openapi/generator/core/fixtures/integration/dto/SendNotificationRequest.java
+++ src/test/java/io/github/rspereiratech/openapi/generator/core/fixtures/integration/dto/SendNotificationRequest.java
@@ -1,3 +1,13 @@
+/*
+ *   ___                   _   ___ ___
+ *  / _ \ _ __  ___ _ _   /_\ | _ \_ _|
+ * | (_) | '_ \/ -_) ' \ / _ \|  _/| |
+ *  \___/| .__/\___|_||_/_/ \_\_| |___|   Generator
+ *       |_|
+ *
+ * MIT License - Copyright (c) 2026 Rui Pereira
+ * See LICENSE in the project root for full license information.
+ */
 package io.github.rspereiratech.openapi.generator.core.fixtures.integration.dto;
 
 public record SendNotificationRequest(String type, String recipient, String subject) {}
